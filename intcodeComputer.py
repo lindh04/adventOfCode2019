406bb2ce9c0e9bc4de52d2655cfa243b862d5c51
Jonatan Lindh <0428lijo@jonkoping.se>
2020-10-01 12:34:23 +0200

    better indexing

--- intcodeComputer.py
+++ intcodeComputer.py
@@ -3,6 +3,7 @@ from typing import Tuple
 
 class IntcodeComputer:
     def __init__(self, intcode, separatorIfString=False) -> None:
+        self.i = 0
         if separatorIfString:
             self.intcode = list(
                 map(int, intcode.split(separatorIfString)))
@@ -10,30 +11,27 @@ class IntcodeComputer:
             self.intcode = intcode
 
     def __call__(self, noun: int = None, verb: int = None):
+        self.i = 0
         data = list(self.intcode)
-        counter = 0
 
         if noun:
             data[1] = noun
         if verb:
             data[2] = verb
 
-        for x in range(len(data)):
-            if counter != 0:
-                counter -= 1
-                continue
-            else:
-                counter, done, data = self.runOpCode(data[x], x, data)
-                if done:
-                    return data[0]
+        while True:
+            done, data = self.runOpCode(data[self.i], data)
+            if done:
+                return data[0]
+            self.i += 1
 
-    def runOpCode(self, opcode: int, index: int, dataList: list) -> Tuple[int, bool, list]:
+    def runOpCode(self, opcode: int, dataList: list) -> Tuple[bool, list]:
         if opcode not in [1, 2, 3, 4, 5, 6, 7, 8]:
             ogOpcode = int(opcode)
             opcode = int(str(opcode)[-2:])
             modes = list(map(int, [i for i in str(ogOpcode)[:-2]]))
             if len(modes) != 3:
-                for x in range(3 - len(modes)):
+                for _ in range(3 - len(modes)):
                     modes.insert(0, 0)
         else:
             modes = None
@@ -47,7 +45,7 @@ class IntcodeComputer:
             7: self.code7,
             8: self.code8,
             99: self.code99,
-        }.get(opcode)(index, dataList, modes)
+        }.get(opcode)(dataList, modes)
 
     def findFromOutput(self, target: int, nounRange: range = range(0, 99), verbRange: range = range(0, 99)):
         for x in nounRange:
@@ -56,7 +54,8 @@ class IntcodeComputer:
                     return x, y
         raise Exception("Unable to find correct values")
 
-    def code1(self, i, dataList, modes) -> Tuple[int, bool, list]:
+    def code1(self, dataList, modes) -> Tuple[bool, list]:
+        i = self.i
         data = list(dataList)
         if not modes:
             data[data[i+3]] = data[data[i+1]] + data[data[i+2]]
@@ -65,9 +64,11 @@ class IntcodeComputer:
             b = data[i+2] if b != 0 else data[data[i+2]]
             c = data[i+1] if c != 0 else data[data[i+1]]
             data[data[i+3]] = b + c
-        return 3, False, data
+        self.i += 3
+        return False, data
 
-    def code2(self, i, dataList, modes) -> Tuple[int, bool, list]:
+    def code2(self, dataList, modes) -> Tuple[bool, list]:
+        i = self.i
         data = list(dataList)
         if not modes:
             data[data[i+3]] = data[data[i+1]] * data[data[i+2]]
@@ -76,17 +77,21 @@ class IntcodeComputer:
             b = data[i+2] if b != 0 else data[data[i+2]]
             c = data[i+1] if c != 0 else data[data[i+1]]
             data[data[i+3]] = b * c
-        return 3, False, data
+        self.i += 3
+        return False, data
 
-    def code3(self, i, dataList, modes) -> Tuple[int, bool, list]:
+    def code3(self, dataList, modes) -> Tuple[bool, list]:
+        i = self.i
         data = list(dataList)
         if not modes:
             data[data[i+1]] = int(input("Opcode 3 (Requesting Input): "))
         else:
             raise Exception("Modes given to opcode 3")
-        return 1, False, data
+        self.i += 1
+        return False, data
 
-    def code4(self, i, dataList, modes) -> Tuple[int, bool, list]:
+    def code4(self, dataList, modes) -> Tuple[bool, list]:
+        i = self.i
         data = list(dataList)
         if not modes:
             print("Opcode 4 (Sending Output): ", data[data[i+1]])
@@ -96,43 +101,51 @@ class IntcodeComputer:
                 print("Opcode 4 (Sending Output): ", data[data[i+1]])
             else:
                 print("Opcode 4 (Sending Output): ", data[i+1])
-        return 1, False, data
+        self.i += 1
+        return False, data
 
-    def code5(self, i, dataList, modes) -> Tuple[int, bool, list]:
+    def code5(self, dataList, modes) -> Tuple[bool, list]:
+        i = self.i
         data = list(dataList)
         if not modes:
             if data[data[i+1]] != 0:
-                counter = data[data[i+2]] - i - 1
+                self.i = data[data[i+2]]
+            else:
+                self.i += 2
         else:
             _, b, c = modes
             b = data[i+2] if b != 0 else data[data[i+2]]
             c = data[i+1] if c != 0 else data[data[i+1]]
 
             if c != 0:
-                counter = b - i
+                self.i = b
+            else:
+                self.i += 2
 
-        return counter, False, data
+        return False, data
 
-    def code6(self, i, dataList, modes) -> Tuple[int, bool, list]:
+    def code6(self, dataList, modes) -> Tuple[bool, list]:
+        i = self.i
         data = list(dataList)
         if not modes:
             if data[data[i+1]] == 0:
-                counter = data[data[i+2]] - i - 1
+                self.i = data[data[i+2]]
             else:
-                counter = 2
+                self.i += 2
         else:
             _, b, c = modes
             b = data[i+2] if b != 0 else data[data[i+2]]
             c = data[i+1] if c != 0 else data[data[i+1]]
 
             if c == 0:
-                counter = b - i
+                self.i = b
             else:
-                counter = 2
+                self.i += 2
 
-        return counter, False, data
+        return False, data
 
-    def code7(self, i, dataList, modes) -> Tuple[int, bool, list]:
+    def code7(self, dataList, modes) -> Tuple[bool, list]:
+        i = self.i
         data = list(dataList)
         if not modes:
             if data[data[i+1]] < data[data[i+2]]:
@@ -149,9 +162,11 @@ class IntcodeComputer:
             else:
                 data[data[i+3]] = 0
 
-        return 3, False, data
+        self.i += 3
+        return False, data
 
-    def code8(self, i, dataList, modes) -> Tuple[int, bool, list]:
+    def code8(self, dataList, modes) -> Tuple[bool, list]:
+        i = self.i
         data = list(dataList)
         if not modes:
             if data[data[i+1]] == data[data[i+2]]:
@@ -168,8 +183,9 @@ class IntcodeComputer:
             else:
                 data[data[i+3]] = 0
 
-        return 3, False, data
+        self.i += 3
+        return False, data
 
-    def code99(self, i, dataList, modes) -> Tuple[int, bool, list]:
+    def code99(self, dataList, modes) -> Tuple[bool, list]:
         data = list(dataList)
-        return 0, True, data
+        return True, data
